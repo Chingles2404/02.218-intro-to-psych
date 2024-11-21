53623707e54181b6fb97f2be23ba2558b338097d
CCH <31771180+Chingles2404@users.noreply.github.com>
2024-11-21 14:12:10 +0800

    Made site initiate tables

--- components/table.tsx
+++ components/table.tsx
@@ -1,66 +1,44 @@
 import { sql } from '@vercel/postgres'
-import { timeAgo } from '@/lib/utils'
-import Image from 'next/image'
-import RefreshButton from './refresh-button'
 import { seed } from '@/lib/seed'
 
 export default async function Table() {
   let data
-  let startTime = Date.now()
 
   try {
-    data = await sql`SELECT * FROM users`
+    data = await sql`SELECT * FROM Students`
   } catch (e: any) {
-    if (e.message.includes('relation "users" does not exist')) {
+    if (e.message.includes('relation "Students" does not exist')) {
       console.log(
-        'Table does not exist, creating and seeding it with dummy data now...'
+        'Table "Students" does not exist, creating it now...'
       )
       // Table is not created yet
       await seed()
-      startTime = Date.now()
-      data = await sql`SELECT * FROM users`
+      data = await sql`SELECT * FROM Students`
     } else {
       throw e
     }
   }
 
-  const { rows: users } = data
-  const duration = Date.now() - startTime
+  try {
+    data = await sql`SELECT * FROM Answers`
+  } catch (e: any) {
+    if (e.message.includes('relation "Answers" does not exist')) {
+      console.log(
+        'Table "Answers" does not exist, creating it now...'
+      )
+      // Table is not created yet
+      await seed()
+      data = await sql`SELECT * FROM Answers`
+    } else {
+      throw e
+    }
+  }
 
   return (
     <div className="bg-white/30 p-12 shadow-xl ring-1 ring-gray-900/5 rounded-lg backdrop-blur-lg max-w-xl mx-auto w-full">
-      <div className="flex justify-between items-center mb-4">
-        <div className="space-y-1">
-          <h2 className="text-xl font-semibold">Recent Users</h2>
-          <p className="text-sm text-gray-500">
-            Fetched {users.length} users in {duration}ms
-          </p>
-        </div>
-        <RefreshButton />
-      </div>
-      <div className="divide-y divide-gray-900/5">
-        {users.map((user) => (
-          <div
-            key={user.name}
-            className="flex items-center justify-between py-3"
-          >
-            <div className="flex items-center space-x-4">
-              <Image
-                src={user.image}
-                alt={user.name}
-                width={48}
-                height={48}
-                className="rounded-full ring-1 ring-gray-900/5"
-              />
-              <div className="space-y-1">
-                <p className="font-medium leading-none">{user.name}</p>
-                <p className="text-sm text-gray-500">{user.email}</p>
-              </div>
-            </div>
-            <p className="text-sm text-gray-500">{timeAgo(user.createdAt)}</p>
-          </div>
-        ))}
-      </div>
+      <p>
+        Tables have been created.
+      </p>
     </div>
   )
 }
