f89c6ada946b49f022c5fb9964040f87aad94018
CCH <31771180+Chingles2404@users.noreply.github.com>
2024-11-21 15:02:10 +0800

    Fixed more syntax errors ;-;

--- lib/seed.ts
+++ lib/seed.ts
@@ -4,19 +4,19 @@ export async function seed() {
   const createTable = await sql`
     CREATE TABLE IF NOT EXISTS Students (
       studentId INT PRIMARY KEY,
-      group INT NOT NULL
+      groupId INT NOT NULL
     );
 
     CREATE TABLE IF NOT EXISTS Answers (
       studentId INT NOT NULL REFERENCES Students(studentId),
       passageId INT NOT NULL,
-      order INT NOT NULL,
+      orderNo INT NOT NULL,
       question1 TEXT NOT NULL,
       question2 TEXT NOT NULL,
       question3 TEXT NOT NULL,
       question4 TEXT NOT NULL,
       question5 TEXT NOT NULL,
-      (studentId, passageId) PRIMARY KEY
+      PRIMARY KEY (studentId, passageId)
     );
     `
 
